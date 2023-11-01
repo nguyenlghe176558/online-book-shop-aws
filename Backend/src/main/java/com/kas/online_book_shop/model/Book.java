e752d30b736f9f7c89138c066f7d5f8bc93f750b
buituansonHE172207 <SonBTHE172207@fpt.edu.vn>
2023-11-01 23:31:52 +0700

    add unit test for book function

--- Backend/src/main/java/com/kas/online_book_shop/model/Book.java
+++ Backend/src/main/java/com/kas/online_book_shop/model/Book.java
@@ -25,7 +25,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
