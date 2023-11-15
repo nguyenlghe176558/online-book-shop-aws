3bd6f8c5f1e5ca65ec291a6da019f7979c069db1
Le Gia Nguyen <nguyenlghe176558@fpt.edu.vn>
2023-11-15 21:22:04 +0700

    Update Slider.jsx

--- Frontend/re-admin/src/pages/slider/Slider.jsx
+++ Frontend/re-admin/src/pages/slider/Slider.jsx
@@ -1,77 +1,79 @@
-import React, { useState, useEffect } from "react";
-import "./slider.scss";
-import Sidebar from "../../components/sidebar/Sidebar";
-import Navbar from "../../components/navbar/Navbar";
+import React, { useState, useEffect } from "react"
+import "./slider.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
 import { DataGrid } from "@mui/x-data-grid";
 import { sliderColumns } from "../../datatablesource";
 import { getAllSliders, deleteSlider } from "../../service/SliderService";
 import { Link } from "react-router-dom";
 
 const Slider = () => {
-  const [data, setData] = useState([]);
-  const [columns, setColumns] = useState([]);
+    const [data, setData] = useState([])
+    const [columns, setColumns] = useState([]);
+  
+    const handleDelete = (id) => {
+        const confirmBox = window.confirm(
+            "Do you really want to delete this slider?"
+        )
+        if (!confirmBox) return
+        deleteSlider(id).then((res) => {
+            window.location.reload()
+        })
+    }
 
-  const handleDelete = (id) => {
-    const confirmBox = window.confirm(
-      "Do you really want to delete this slider?"
-    );
-    if (!confirmBox) return;
-    deleteSlider(id).then((res) => {
-      window.location.reload();
-    });
-  };
+    const actionColumn = [
+        {
+            field: "action",
+            headerName: "Action",
+            width: 230,
+            renderCell: (params) => {
+                return (
+                    <div className="cellAction">
+                        <Link to={`/sliders/${params.row.id}`} style={{ textDecoration: "none" }}>
+                            <div className="viewButton">Update</div>
+                        </Link>
+                        <div
+                            className="deleteButton"
+                            onClick={() => handleDelete(params.row.id)}
+                        >
+                            Delete
+                        </div>
+                    </div>
+                );
+            },
+        },
+    ];
 
-  const actionColumn = {
-    field: "actions",
-    headerName: "Actions",
-    width: 150,
-    renderCell: (params) => {
-      return (
-        <>
-          <Link to={`/sliders/${params.row._id}`} className="link">
-            <button className="datatableEdit">Edit</button>
-          </Link>
-          <button
-            className="datatableDelete"
-            onClick={() => handleDelete(params.row._id)}
-          >
-            Delete
-          </button>
-        </>
-      );
-    },
-  };
+    useEffect(() => {
+        getAllSliders().then((res) => {
+            setData(res.data)
+            setColumns(sliderColumns.concat(actionColumn))
+        })
+    }, [])
 
-  useEffect(() => {
-    getAllSliders().then((res) => {
-      setData(res.data);
-      setColumns(sliderColumns.concat(actionColumn));
-    });
-  }, []);
-
-  return (
-    <div className="list">
-      <Sidebar />
-      <div className="listContainer">
-        <Navbar />
-        <div className="datatable">
-          <div className="datatableTitle">
-            Sliders
-            <Link to={`/sliders/new`} className="link">
-              Add New
-            </Link>
-          </div>
-          <DataGrid
-            className="datagrid"
-            rows={data}
-            columns={columns}
-            pageSize={9}
-            rowsPerPageOptions={[9]}
-          />
+    return (
+        <div className="list">
+            <Sidebar />
+            <div className="listContainer">
+                <Navbar />
+                <div className="datatable">
+                    <div className="datatableTitle">
+                        Manage Sliders
+                        <Link to={`/sliders/new`} className="link">
+                            Add New Slider
+                        </Link>
+                    </div>
+                    <DataGrid
+                        className="datagrid"
+                        rows={data}
+                        columns={columns}
+                        pageSize={9}
+                        rowsPerPageOptions={[9]}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Slider;
+export default Slider
